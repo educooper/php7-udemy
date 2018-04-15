069b155b4b5bc988b7a21c02771141131bab24ed
Edu Cooper <edusardo@uninove.edu.br>
2018-04-15 08:27:09 -0300

    Data

---
+++ date/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+
+echo date("d/m/Y H:i:s" , 1523738787);
+
+echo "<br>";
+
+echo time();
+ ?>
---
+++ date/exemplo-02.php
@@ -0,0 +1,11 @@
+<?php
+
+$ts = strtotime("2001-09-11");
+
+echo "<br>";
+
+echo date("d/m/Y l", $ts );
+
+echo "<br/>";
+
+?>
